44a2dc9efa1a50906b97164831d40d0fde02c61b
Your Name <you@example.com>
2026-06-12 14:51:08 -0500

    Initial Commit

---
+++ SolarSystem/SolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemApp.swift
+//  SolarSystem
+//
+//  Created by Raul Dozal on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
